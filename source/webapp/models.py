980f82c64f4380e9b47a46e6a97b5d5dad94d537
Artur <metis130383@gmail.com>
2020-10-12 15:57:31 +0600

    Выполнен  втрой этап

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -52,9 +52,14 @@ class Comment(models.Model):
                                related_name='comments', verbose_name='Автор')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    like_count = models.IntegerField(verbose_name='Счетчик лайков', default=0)
 
     def __str__(self):
         return self.text[:20]
+
+    def liked_by(self, user):
+        likes = self.likes.filter(user=user)
+        return likes.count() > 0
 
     class Meta:
         verbose_name = 'Комментарий'
